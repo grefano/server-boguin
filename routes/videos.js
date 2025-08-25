83450fd21e4e2dc07f0a0bd04ffdef0e27e9bc6f
grefano <gabrielnieed@gmail.com>
2025-08-25 20:13:37 -0300

    change endpoint get videos order
    get videos/feed and get videos/id

--- routes/videos.js
+++ routes/videos.js
@@ -8,83 +8,14 @@ const cloudinary = require('cloudinary').v2
 const upload = multer({
     storage: multer.memoryStorage(),
     limits: { fileSize: limit_size * 1024 * 1024 } // 100 MB limit
-})
+})    
 
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
-})
-
-router.get('/:id', async (req, res) => {
-    console.log('get video')
-    const { id } = req.params
-    try {
-        const video = await getVideo(id)
-        res.status(200).json(video)
-    } catch (error) {
-        console.error(`erro ao buscar video ${id}`, error)
-        res.status(500).json({ error: 'erro ao buscar vídeos'})
-    }
-})
-
-router.get('/feed', async (req, res) => {
-    console.log('feed')
-    try {
-        const videos = await getVideos()
-        res.status(200).json(videos)
-    } catch (error){
-        console.error('erro ao buscar videos do feed', error)
-        res.status(500).json({ error: 'erro ao buscar vídeos'})
-    }    
 })    
-
-router.get('/users/:userId', async (req, res) => {
-    console.log('my-videos')
-    try{
-        console.log(req.params)
-        const { userId } = req.params
-        const videos = await getChannelVideos(userId)
-        res.status(200).json(videos)
-    } catch (error){
-        console.error('erro ao buscar vídeos', error)
-        res.status(500).json({error:'erro ao buscar vídeos'})
-    }
-})    
-
-
-
-router.delete('/:id', async (req, res) => {
-    try {
-        console.log('delete')
-        const { id } = req.params
-        const video = await getVideo(id)
-        if (!video) {
-            return res.status(404).json({ error: 'Video not found' })
-        }    
-        await cloudinary.uploader.destroy(id, { resource_type: 'video', invalidate: true }, function(error, result) {
-            if (error) {
-                return res.status(500).json({ error: 'Error deleting video from Cloudinary' })
-            }    
-        })    
-        await cloudinary.uploader.destroy(video.id_thumb, { resource_type: 'image', invalidate: true }, function(error, result) {
-            if (error) {
-                return res.status(500).json({ error: 'Error deleting thumbnail from Cloudinary' })
-            }    
-        })    
-        
-        await deleteVideo(id)
-
-        res.status(200).json({ message: 'Video and thumbnail deleted successfully' })
-    } catch (error) {
-        console.error('Error deleting video:', error)
-        res.status(500).json({ error: 'erro no server ao excluir video' })
-    }    
-})    
-
-
-
 
 router.post('/', upload.fields([
     { name: 'video', maxCount: 1 },
@@ -142,6 +73,77 @@ router.post('/', upload.fields([
         console.error('erro no upload do video: ', error)
     }
 })
+
+
+
+router.get('/feed', async (req, res) => {
+    console.log('feed')
+    try {
+        const videos = await getVideos()
+        res.status(200).json(videos)
+    } catch (error){
+        console.error('erro ao buscar videos do feed', error)
+        res.status(500).json({ error: 'erro ao buscar vídeos'})
+    }        
+})        
+
+router.get('/:id', async (req, res) => {
+    console.log('get video')
+    const { id } = req.params
+    try {
+        const video = await getVideo(id)
+        res.status(200).json(video)
+    } catch (error) {
+        console.error(`erro ao buscar video ${id}`, error)
+        res.status(500).json({ error: 'erro ao buscar vídeos'})
+    }    
+})    
+
+
+router.get('/users/:userId', async (req, res) => {
+    console.log('my-videos')
+    try{
+        console.log(req.params)
+        const { userId } = req.params
+        const videos = await getChannelVideos(userId)
+        res.status(200).json(videos)
+    } catch (error){
+        console.error('erro ao buscar vídeos', error)
+        res.status(500).json({error:'erro ao buscar vídeos'})
+    }    
+})        
+
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+        console.log('delete')
+        const { id } = req.params
+        const video = await getVideo(id)
+        if (!video) {
+            return res.status(404).json({ error: 'Video not found' })
+        }        
+        await cloudinary.uploader.destroy(id, { resource_type: 'video', invalidate: true }, function(error, result) {
+            if (error) {
+                return res.status(500).json({ error: 'Error deleting video from Cloudinary' })
+            }        
+        })        
+        await cloudinary.uploader.destroy(video.id_thumb, { resource_type: 'image', invalidate: true }, function(error, result) {
+            if (error) {
+                return res.status(500).json({ error: 'Error deleting thumbnail from Cloudinary' })
+            }        
+        })        
+        
+        await deleteVideo(id)
+
+        res.status(200).json({ message: 'Video and thumbnail deleted successfully' })
+    } catch (error) {
+        console.error('Error deleting video:', error)
+        res.status(500).json({ error: 'erro no server ao excluir video' })
+    }        
+})        
+
+
 
 
 module.exports = router
