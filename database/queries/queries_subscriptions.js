5da06056e95c71d21e4d923ef68cd9f1e2ae3fbf
grefano <gabrielnieed@gmail.com>
2025-08-26 17:12:57 -0300

    await supabase add sub

--- database/queries/queries_subscriptions.js
+++ database/queries/queries_subscriptions.js
@@ -4,7 +4,7 @@ const supabase = require('../../db')
 
 async function addSubscription(owner_id, subject_id, type="all"){
     try {
-        const {data, error} = supabase.from('subscriptions').insert([{owner_id, subject_id, type}])
+        const {data, error} = await supabase.from('subscriptions').insert([{owner_id, subject_id, type}]).select()
         console.log(`data ${data}`)
         console.log(`error ${error}`)
         return error ? error : data
