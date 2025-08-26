5729260a7b7ff33bd47aba56d4cfdb15309e7739
grefano <gabrielnieed@gmail.com>
2025-08-26 17:07:00 -0300

    log req query on endpoint sub add

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -7,6 +7,7 @@ router.post('/', async (req, res) => {
     const {owner_id, subject_id, type} = req.query
     console.log(`add sub`)
     console.log(req.query)
+    console.log(`${owner_id} ${subject_id} ${type}`)
     const result = await addSubscription(owner_id, subject_id, type)
     console.log(result)
     res.status(200)
